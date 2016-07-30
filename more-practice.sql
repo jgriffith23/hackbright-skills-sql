29a0467fed30c7d2af028627e5def59ed8e614c8
Jennifer <jennifer.gd42@gmail.com>
2016-07-30 21:55:04 +0000

    Finished INSERT exercise from More Practice.

--- more-practice.sql
+++ more-practice.sql
@@ -4,6 +4,26 @@
 
 -- INSERT
 
+-- Practice adding rows to a database. These two statements add 
+-- a 2015 Chevrolet Malibu entry and a 2015 Subaru Outback entry.
+-- After executing, the following output is obtainable:
+
+-- cars=# SELECT * FROM models WHERE year = 2015;
+--  id | year | brand_name |  name
+-- ----+------+------------+---------
+--  48 | 2015 | Chevrolet  | Malibu
+--  49 | 2015 | Subaru     | Outback
+
+BEGIN;
+INSERT INTO models (year, brand_name, name)
+VALUES (2015, 'Chevrolet', 'Malibu');
+COMMIT;
+
+BEGIN
+INSERT INTO models (year, brand_name, name)
+VALUES (2015, 'Subaru', 'Outback');
+COMMIT;
+
 
 -- CREATE TABLE
 
